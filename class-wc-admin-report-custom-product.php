37467d1e6e0e93838c29157abd9c33a8beeee129
Shirkit <shirkit@gmail.com>
2020-01-08 12:51:54 -0300

    Indentation implementation

--- class-wc-admin-report-custom-product.php
+++ class-wc-admin-report-custom-product.php
@@ -1,59 +1,59 @@
 <?php
 class WC_POS_Report_Custom_Product_Sales extends WC_Admin_Report
 {
-    /**
-     * Output the report.
-     */
-    public function output_report()
-    {
-        $ranges = array(
-            'year' => __('Year', 'woocommerce') ,
-            'last_month' => __('Last month', 'woocommerce') ,
-            'month' => __('This month', 'woocommerce') ,
+	/**
+	 * Output the report.
+	 */
+	public function output_report()
+	{
+		$ranges = array(
+			'year' => __('Year', 'woocommerce') ,
+			'last_month' => __('Last month', 'woocommerce') ,
+			'month' => __('This month', 'woocommerce') ,
 			'7day' => __('Last 7 days', 'woocommerce')
-        );
-        $current_range = !empty($_GET['range']) ? sanitize_text_field($_GET['range']) : 'month';
-        if (!in_array($current_range, array(
-            'custom',
-            'year',
-            'last_month',
-            '7day'
-        )))
-        {
-            $current_range = 'month';
-        }
-        $this->check_current_range_nonce($current_range);
-        $this->calculate_current_range($current_range);
-        $hide_sidebar = true;
-        include (WC()->plugin_path() . '/includes/admin/views/html-report-by-date.php');
-    }
-    /**
-     * Get the main chart.
-     */
-    public function get_main_chart()
-    {
-        global $wpdb;
+		);
+		$current_range = !empty($_GET['range']) ? sanitize_text_field($_GET['range']) : 'month';
+		if (!in_array($current_range, array(
+			'custom',
+			'year',
+			'last_month',
+			'7day'
+		)))
+		{
+			$current_range = 'month';
+		}
+		$this->check_current_range_nonce($current_range);
+		$this->calculate_current_range($current_range);
+		$hide_sidebar = true;
+		include (WC()->plugin_path() . '/includes/admin/views/html-report-by-date.php');
+	}
+	/**
+	 * Get the main chart.
+	 */
+	public function get_main_chart()
+	{
+		global $wpdb;
 		$custom_product_id = get_option('wc_pos_custom_product_id');
-        $result = $this->get_order_report_data(array(
-            'data' => array(
-                '_product_id' => array(
-                    'type' => 'order_item_meta',
-                    'order_item_type' => 'line_item',
-                    'function' => '',
-                    'name' => 'custom_product_id'
-                ),
-                '_line_total' => array(
-                    'type' => 'order_item_meta',
-                    'order_item_type' => 'line_item',
-                    'function' => '',
-                    'name' => 'price'
-                ),
+		$result = $this->get_order_report_data(array(
+			'data' => array(
+				'_product_id' => array(
+					'type' => 'order_item_meta',
+					'order_item_type' => 'line_item',
+					'function' => '',
+					'name' => 'custom_product_id'
+				),
+				'_line_total' => array(
+					'type' => 'order_item_meta',
+					'order_item_type' => 'line_item',
+					'function' => '',
+					'name' => 'price'
+				),
 				'order_item_id' => array(
-                    'type' => 'order_item',
-                    'order_item_type' => 'line_item',
-                    'function' => '',
-                    'name' => 'product_item_id'
-                ),
+					'type' => 'order_item',
+					'order_item_type' => 'line_item',
+					'function' => '',
+					'name' => 'product_item_id'
+				),
 				'order_item_name' => array(
 					'type' => 'order_item',
 					'order_item_type' => 'line_item',
@@ -66,7 +66,7 @@ class WC_POS_Report_Custom_Product_Sales extends WC_Admin_Report
 					'function' => '',
 					'name' => 'order_id',
 				)
-            ),
+			),
 			'where_meta' => array(
 				array(
 					'type' => 'order_item_meta',
@@ -75,37 +75,37 @@ class WC_POS_Report_Custom_Product_Sales extends WC_Admin_Report
 					'operator' => '=',
 				)
 			),
-            'query_type' => 'get_results',
-            'filter_range' => true,
-        ));
+			'query_type' => 'get_results',
+			'filter_range' => true,
+		));
 ?>
-    <table class="widefat">
-      <thead>
-          <tr>
-              <th><strong>Product Name</strong></th>
-              <th><strong>Product Price</strong></th>
-              <th><strong>Order ID</strong></th>
-          </tr>
-      </thead>
-      <tbody>
-          <?php foreach ($result as $order)
-        {
+<table class="widefat">
+  <thead>
+    <tr>
+      <th><strong>Product Name</strong></th>
+      <th><strong>Product Price</strong></th>
+      <th><strong>Order ID</strong></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($result as $order)
+		{
 			$button = array(
 				'url'    => admin_url( 'post.php?post=' . $order->order_id . ' &action=edit' ),
 				'name'   => __( 'View order', 'woocommerce' ),
 				'action' => 'view',
 			);
 ?>
-          <tr>
-              <td><?php echo $order->name; ?></td>
-			  <td><?php echo $order->price; ?></td>
-			  <td><?php printf( '<a class="button tips %s" href="%s" data-tip="%s">%s</a>', esc_attr( $button['action'] ), esc_url( $button['url'] ), esc_attr( $button['name'] ), esc_attr( $button['name'] ) ); ?></td>
-          </tr>
-          <?php
-        } ?>
-      </tbody>
-    </table>
+    <tr>
+      <td><?php echo $order->name; ?></td>
+      <td><?php echo $order->price; ?></td>
+      <td><?php printf( '<a class="button tips %s" href="%s" data-tip="%s">%s</a>', esc_attr( $button['action'] ), esc_url( $button['url'] ), esc_attr( $button['name'] ), esc_attr( $button['name'] ) ); ?></td>
+    </tr>
     <?php
-    }
+		} ?>
+  </tbody>
+</table>
+<?php
+	}
 }
 ?>
--- class-wc-admin-report-order-comments.php
+++ class-wc-admin-report-order-comments.php
@@ -1,83 +1,83 @@
 <?php
 class WC_Report_Orders_With_Comments extends WC_Admin_Report
 {
-    /**
-     * Output the report.
-     */
-    public function output_report()
-    {
-        $ranges = array(
-            'year' => __('Year', 'woocommerce') ,
-            'last_month' => __('Last month', 'woocommerce') ,
-            'month' => __('This month', 'woocommerce') ,
-        );
-        $current_range = !empty($_GET['range']) ? sanitize_text_field($_GET['range']) : 'month';
-        if (!in_array($current_range, array(
-            'custom',
-            'year',
-            'last_month',
-            '7day'
-        )))
-        {
-            $current_range = 'month';
-        }
-        $this->check_current_range_nonce($current_range);
-        $this->calculate_current_range($current_range);
-        $hide_sidebar = true;
-        include (WC()->plugin_path() . '/includes/admin/views/html-report-by-date.php');
-    }
-    /**
-     * Get the main chart.
-     */
-    public function get_main_chart()
-    {
-        global $wpdb;
+	/**
+	 * Output the report.
+	 */
+	public function output_report()
+	{
+		$ranges = array(
+			'year' => __('Year', 'woocommerce') ,
+			'last_month' => __('Last month', 'woocommerce') ,
+			'month' => __('This month', 'woocommerce') ,
+		);
+		$current_range = !empty($_GET['range']) ? sanitize_text_field($_GET['range']) : 'month';
+		if (!in_array($current_range, array(
+			'custom',
+			'year',
+			'last_month',
+			'7day'
+		)))
+		{
+			$current_range = 'month';
+		}
+		$this->check_current_range_nonce($current_range);
+		$this->calculate_current_range($current_range);
+		$hide_sidebar = true;
+		include (WC()->plugin_path() . '/includes/admin/views/html-report-by-date.php');
+	}
+	/**
+	 * Get the main chart.
+	 */
+	public function get_main_chart()
+	{
+		global $wpdb;
 		$custom_product_id = get_option('wc_pos_custom_product_id');
-        $result = $this->get_order_report_data(array(
-            'data' => array(
-                'order_comments' => array(
-                    'type' => 'meta',
-                    'order_item_type' => 'line_item',
-                    'function' => '',
-                    'name' => 'comments'
-                ),
+		$result = $this->get_order_report_data(array(
+			'data' => array(
+				'order_comments' => array(
+					'type' => 'meta',
+					'order_item_type' => 'line_item',
+					'function' => '',
+					'name' => 'comments'
+				),
 				'order_id' => array(
 					'type' => 'order_item',
 					'order_item_type' => 'line_item',
 					'function' => '',
 					'name' => 'order_id',
 				)
-            ),
+			),
 			'group_by' => 'order_id',
-            'query_type' => 'get_results',
-            'filter_range' => true,
-        ));
+			'query_type' => 'get_results',
+			'filter_range' => true,
+		));
 ?>
-    <table class="widefat">
-      <thead>
-          <tr>
-              <th><strong>Comments</strong></th>
-              <th><strong>Order ID</strong></th>
-          </tr>
-      </thead>
-      <tbody>
-          <?php foreach ($result as $order)
-        {
+<table class="widefat">
+  <thead>
+    <tr>
+      <th><strong>Comments</strong></th>
+      <th><strong>Order ID</strong></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($result as $order)
+		{
 			$button = array(
 				'url'    => admin_url( 'post.php?post=' . $order->order_id . ' &action=edit' ),
 				'name'   => __( 'View order', 'woocommerce' ),
 				'action' => 'view',
 			);
 ?>
-          <tr>
-              <td><?php echo $order->comments; ?></td>
-			  <td><?php printf( '<a class="button tips %s" href="%s" data-tip="%s">%s</a>', esc_attr( $button['action'] ), esc_url( $button['url'] ), esc_attr( $button['name'] ), esc_attr( $button['name'] ) ); ?></td>
-          </tr>
-          <?php
-        } ?>
-      </tbody>
-    </table>
+    <tr>
+      <td><?php echo $order->comments; ?></td>
+      <td><?php printf( '<a class="button tips %s" href="%s" data-tip="%s">%s</a>', esc_attr( $button['action'] ), esc_url( $button['url'] ), esc_attr( $button['name'] ), esc_attr( $button['name'] ) ); ?></td>
+    </tr>
     <?php
-    }
+		} ?>
+  </tbody>
+</table>
+<?php
+	}
 }
 ?>
